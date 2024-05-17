49aa3190aac4df86f128ff7a1ca5b545e94c9238
Stefano Galassi <stefano.galassi@gotoko.co.id>
2024-05-17 18:31:31 +0800

    up compose

--- register_telegram_hook.sh
+++ register_telegram_hook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl -F "url=https://4f79-103-100-175-237.ngrok-free.app/webhook" https://api.telegram.org/bot6833202420:AAF32TethdiI0d0i6kdzlK2_IBibkLu5sCg/setWebhook
+curl -F "url=https://telegrambot.revisbali.com/webhook" https://api.telegram.org/bot6833202420:AAF32TethdiI0d0i6kdzlK2_IBibkLu5sCg/setWebhook
 curl https://api.telegram.org/bot6833202420:AAF32TethdiI0d0i6kdzlK2_IBibkLu5sCg/getWebhookInfo
